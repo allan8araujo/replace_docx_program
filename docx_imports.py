3456d94782c010b3eb0591724c13bc53d5541695
Allan Araújo <77680596+allan8araujo@users.noreply.github.com>
2021-05-31 23:45:40 -0300

    Update docx_imports.py

--- docx_imports.py
+++ docx_imports.py
@@ -1,30 +1,27 @@
 import docx
 from docx.shared import Pt
 from tkinter_imports import replace_paragraph, replace_table, path_entry, var, word_entry_paragraph,word_entry_tables
-'''here you need to put insted {YOUR ABSOLUTE PATH} the absolute path of your docx file, like 'c:/[...]/arquive.docx' '''
 
+#here you need to put insted {YOUR ABSOLUTE PATH} the absolute path of your docx file, like 'c:/[...]/arquive.docx'
 doc = docx.Document('{YOUR ABSOLUTE PATH}')
 texto = ''
 tabelas = ''
 paragraphs_num = (len(doc.paragraphs))
 tables_num = len(doc.tables)
 
-'''setting up font, styles and stuff'''
+#setting up font, styles and stuff
 font = doc.styles['Normal'].font
 font.name = 'Arial'
 font.size = Pt(10)
 
-
+#this function is reading the paragraph 1 per 1 
 def paragraph_reading():
     global doc
     global texto
     for n in range(0, paragraphs_num):
         texto += str(doc.paragraphs[n].text) + '\n'
 
-
-paragraph_reading()
-phrase=''
-
+# if you want to delete a word from a table into a word.docx file you can put the word insted of 'FERIADO'
 def feriados_retirar():
     global texto
     global replace_to_paragraph
@@ -35,6 +32,7 @@ def feriados_retirar():
             for col in table.columns:
                 for cell in col.cells:
                     for p in cell.paragraphs:
+                        #here you can replace the p.text[0:n] search list
                         if p.text[0:7] == 'FERIADO':
                             p.text = ''
                         texto += p.text
@@ -42,6 +40,9 @@ def feriados_retirar():
         print('Done')
     else:
         return print('not done')
+    
+#search inside tables and replace the word, the text variable is pass into the tkinter interface
+
 def paragraph_replace():
     global texto
     global replace_to_paragraph
@@ -67,6 +68,8 @@ def paragraph_replace():
                         p.text = str(p.text).replace(str(word_to_find_table),str(replace_to_tables))
                     texto += p.text
                     texto += '\n'
+    
+    #calls the functions
     doc_gravacao = docx.Document()
     global paraObject
     paraObject = doc_gravacao.add_paragraph()
