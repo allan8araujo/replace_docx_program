5af8ce36ed4691d86345d0c86b578d7e296a8ab1
Allan Araújo <77680596+allan8araujo@users.noreply.github.com>
2021-06-01 22:59:26 -0300

    Add files via upload

--- tkinter_imports.py
+++ tkinter_imports.py
@@ -1,4 +1,6 @@
 from tkinter import Tk, Label, Entry, Button, LabelFrame, IntVar, Checkbutton
+import docx
+from docx.shared import Pt
 
 janela = Tk()
 janela.title('Words replacer')
@@ -33,6 +35,12 @@ replace_paragraph.grid(column=2, row=1)
 replace_table = Entry(frame_1)
 replace_table.grid(column=2, row=2)
 
+doc = docx.Document('PONTOS_DEZEMBRO_2020.docx')
+texto, tabelas = "", ''
+
+paragraphs_num = (len(doc.paragraphs))
+tables_num = len(doc.tables)
+
 path_entry = Entry(frame_1)
 path_entry.grid(column=1, row=3)
 word= ''
@@ -40,9 +48,64 @@ var = IntVar()
 checkbox = Checkbutton(frame_1, text=f'Clean this {word} word', variable=var, bg='slategray1')
 checkbox.grid(column=2, row=7)
 
-from docx_imports import feriados_retirar, paragraph_replace
-
 botao_data = Button(frame_1, width='25', height='1', text='Do it!', bg='slategray3',
                     command=lambda: [feriados_retirar(), paragraph_replace()])
 botao_data.grid(column=1, row=7)
+
+def paragraph_reading():
+    global doc
+    global texto
+    for n in range(0, paragraphs_num):
+        texto += str(doc.paragraphs[n].text) + '\n'
+
+
+paragraph_reading()
+phrase=''
+
+def feriados_retirar():
+    global texto
+    global replace_to_paragraph
+    global replace_to_tables
+    global var
+    if var.get() == 1:
+        for table in doc.tables:
+            for col in table.columns:
+                for cell in col.cells:
+                    for p in cell.paragraphs:
+                        if p.text[0:7] == 'FERIADO':
+                            p.text = ''
+                        texto += p.text
+                        texto += '\n'
+        print('Done')
+    else:
+        return print('not done')
+def paragraph_replace():
+    global texto
+    global replace_to_paragraph
+    global replace_to_tables
+    global path
+    global phrase
+    replace_to_paragraph = replace_paragraph.get()
+    replace_to_tables = replace_table.get()
+    path = path_entry.get()
+    path = str(path)
+    word_to_find_paragraph = str(word_entry_paragraph.get())
+    word_to_find_table=str(word_entry_tables.get())
+    for n in range(0, paragraphs_num):
+        if doc.paragraphs[n].text == "":
+            continue
+        elif word_to_find_paragraph in doc.paragraphs[n].text:
+            doc.paragraphs[n].text=str(doc.paragraphs[n].text).replace(str(word_to_find_paragraph),str(replace_to_paragraph))
+    for table in doc.tables:
+        for col in table.columns:
+            for cell in col.cells:
+                for p in cell.paragraphs:
+                    if word_to_find_table in p.text:
+                        p.text = str(p.text).replace(str(word_to_find_table),str(replace_to_tables))
+                    texto += p.text
+                    texto += '\n'
+    doc_gravacao = docx.Document()
+    global paraObject
+    paraObject = doc_gravacao.add_paragraph()
+    doc.save(path + '.docx')
 janela.mainloop()
